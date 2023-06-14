253ebf07c59a36fb86a83395da7dba0f16769f65
Evelina <evelina.venckute@swedbank.lt>
2023-06-14 15:00:43 +0300

    set, update, mutate

--- src/app/signals/signals.component.ts
+++ src/app/signals/signals.component.ts
@@ -1,23 +1,25 @@
-import { NgFor } from '@angular/common';
-import { Component , signal} from '@angular/core';
+import { NgFor } from "@angular/common";
+import { Component, signal } from "@angular/core";
 
 @Component({
-  selector: 'app-signals',
-  templateUrl: './signals.component.html',
+  selector: "app-signals",
+  templateUrl: "./signals.component.html",
   standalone: true,
   imports: [NgFor],
 })
 export class SignalsComponent {
-  actions: string[] = [];
+  actions = signal<string[]>([]);
   counter = signal(0);
 
   increment() {
-    this.counter++;
-    this.actions.push('INCREMENT');
+    // this.counter.update((oldcounter) => oldcounter + 1);
+    this.counter.set(this.counter()+1)
+    this.actions.mutate((oldvalue) => oldvalue.push("INCREMENT"));
+    // this.actions.push("INCREMENT");
   }
 
   decrement() {
-    this.counter--;
-    this.actions.push('DECREMENT');
+    this.counter.update((oldcounter) => oldcounter - 1);
+    this.actions.update((oldActions) => [...oldActions, "DECREMENT"]);
   }
 }
